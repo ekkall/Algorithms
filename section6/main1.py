1b6348d467e6a29c0c9ca3829e5b4be63f741760
ekkall <ud8664@gmail.com>
2021-08-11 20:59:07 +0900

    21/08/11

---
+++ section6/main1.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def DFS(x):
+    if x == 0:
+        return
+    else:
+        DFS(x // 2)
+        print(x % 2, end='')
+
+
+n = int(input())
+DFS(n)
+
+'''
+# 이진수를 재귀함수로 구하는 문제
+
+'''
