1ae0306c40f2ad7c773eed04786b16a640dbea13
ekkall <ud8664@gmail.com>
2021-08-15 19:54:10 +0900

    21/08/15

--- section6/main5.py
+++ section6/main5.py
@@ -22,9 +22,14 @@ if __name__ == "__main__":
     DFS(0, 0, 0)
     print(result)
 
+
 '''
 #바둑이 승차
 
 dfs 활용
+
+* 현재의 sum 에 total-tsum(현재 합하지 않은 바둑이들의 총 합)을 더한 것과, result를 비교함으로써 cut edge하면 시간복잡도 더 줄일 수 있음.
+* ' result = ' 는 함수 내에서 지역변수를 선언하는 것이므로
+global result << 꼭 지역변수인지 / 전역변수인지 명시하기!!
 
 '''
