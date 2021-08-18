ce412d17904e8129654b04a356d6a27fa29f480e
ekkall <ud8664@gmail.com>
2021-08-18 20:12:02 +0900

    21/08/18

---
+++ section6/main8.py
@@ -0,0 +1,30 @@
+def DFS(L):
+    global cnt
+    if L == m:
+        for i in range(m):
+            print(res[i], end=' ')
+        print()
+        cnt += 1
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                ch[i] = 1
+                res[L] = i
+                DFS(L + 1)
+                ch[i] = 0
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    res = [0] * m
+    ch = [0] * (n + 1)
+    cnt = 0
+    DFS(0)
+    print(cnt)
+
+'''
+#순열 구하기
+
+DFS 활용
+
+'''
