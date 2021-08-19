d9cb7ab547369cf28216e0683f3def6463400f51
ekkall <ud8664@gmail.com>
2021-08-19 19:49:37 +0900

    21/08/19

--- section6/main2.py
+++ section6/main2.py
@@ -2,9 +2,11 @@ def DFS(v):
     if v > 7:
         return
     else:
-        print(v, end=' ') #전위순회
+       # print(v, end=' ') #전위순회
         DFS(v * 2)
+       # print(v, end=' ') #중위순회
         DFS(v * 2 + 1)
+        print(v, end=' ') #후위순회
 
 
 if __name__ == "__main__":
